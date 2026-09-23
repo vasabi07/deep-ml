4ad8bf8ec40c7df55d1766ebd5db04feb684e3cf
vasabi07 <60343336+vasabi07@users.noreply.github.com>
2026-09-23 20:28:22 +0000

    Solve: Implement the Tanh Activation Function

---
+++ problems/0264-implement-the-tanh-activation-function/solution.py
@@ -0,0 +1,15 @@
+import math
+
+def tanh(x: float) -> float:
+	"""
+	Implements the Tanh (hyperbolic tangent) activation function.
+
+	Args:
+		x (float): Input value
+
+	Returns:
+		float: The tanh of the input, rounded to 4 decimal places
+	"""
+	# Your code here
+	tanh_x = (math.exp(x)-math.exp(-x))/(math.exp(x)+ math.exp(-x))
+	return tanh_x
